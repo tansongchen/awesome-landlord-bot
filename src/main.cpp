9b8bc65510e1b572676eaeb045cabb2afe1d4e3d
past-urbanist <70696479+past-urbanist@users.noreply.github.com>
2021-05-28 16:04:12 +0800

    Keer (#8)
    * temp commit
    
    * add my code to keer about defnesive strategies
    
    * details revision
    
    Co-authored-by: Songchen Tan <songchen@mit.edu>

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,16 @@ int main() {
   read();
   cout << "read successful" << endl;
   evaluator = simple_evaluator;
-  attacking_sequence = {solo_selector};
-  defending_sequence = {pass_selector};
+  attacking_sequence = {attacking_absolute_dominant_selector, one_shot_selector,
+                        full_enumeration_selector, solo_selector};
+  defending_sequence = {boom_terminator_selector, same_category_selector,
+                        defending_absolute_dominant_selector, pass_selector};
   cout << "strats load successful" << endl;
   Counter counter(myCards);
   if (stage == Stage::BIDDING) {
     int suggestion = suggest(&counter);
-    int bid_value = bidInput.empty() || suggestion > bidInput.back() ? bid_value : 0;
+    int bid_value =
+        bidInput.empty() || suggestion > bidInput.back() ? bid_value : 0;
     bid(bid_value);
   } else if (stage == Stage::PLAYING) {
     cout << "play start" << endl;
@@ -20,7 +23,8 @@ int main() {
     cout << last_hand_counter << endl;
     Hand last_hand(last_hand_counter);
     cout << last_hand << endl;
-    Hand hand = last_hand == pass ? attack(&counter) : defend(&counter, last_hand);
+    Hand hand =
+        last_hand == pass ? attack(&counter) : defend(&counter, last_hand);
     cout << hand << endl;
     Counter counter(hand.get_counter());
     Group group(counter.get_group(myCards));
--- test/strategy/defend_test.cpp
+++ test/strategy/defend_test.cpp
@@ -30,33 +30,28 @@ TEST_CASE("Strategy 2: returning same category", "[0]") {
   defending_sequence = {boom_terminator_selector, same_category_selector,
                         pass_selector};
 
-  // SECTION("Case 2.1") {
-  //   Counter counter({4, 6, 40, 41, 42, 43});  // 4 4 K K K K
-  //   Hand last_hand = {0, 1, 2, 3};            // 3 3 3 3
-  //   // REQUIRE(evaluate(&counter) == (10 + 7) + (1 - 7););
-  //   REQUIRE(defend(&counter, last_hand) == Hand(10, 4, 1)); // Level: 65535,
-  //   Length: 0, Size: 0, Cosize: 0, Attached ERROR
-  // }
+  SECTION("Case 2.1") {
+    Counter counter({4, 6, 40, 41, 42, 43});  // 4 4 K K K K
+    Hand last_hand(Counter({0, 1, 2, 3}));    // 3 3 3 3
+    // REQUIRE(evaluate(&counter) == (10 + 7) + (1 - 7););
+    REQUIRE(defend(&counter, last_hand) == Hand(10, 1, 4));
+  }
   SECTION("Case 2.2") {
-    Counter counter({3, 29, 41, 42, 43, 44, 46, 47});  // 3 10 K K K A A A
-    Hand last_hand(Counter({0, 1, 2, 22}));            // 3 3 3 8
-    Counter myturn({3, 41, 42, 43});                   // K K K 3
-    // REQUIRE(evaluate(&counter) == (10 + 1) / 2 + (11 + 1) / 2);
-    REQUIRE(defend(&counter, last_hand) == Hand(myturn));
+    Counter counter({0, 4, 8, 12, 16, 20, 24, 28});  // 3 5 6 7 8 9 10
+    Hand last_hand(Counter({1, 5, 9, 13, 17}));      // 3 4 5 6 7
+    REQUIRE(defend(&counter, last_hand) == Hand(Counter({8, 12, 16, 20, 24})));
   }
   SECTION("Case 2.3") {
-    Counter counter(
-        {0, 4, 8, 10, 12, 13, 14, 16, 20, 24});  // 3 4 5 5 6 6 6 7 8 9
-    Hand last_hand(Counter({1, 5, 9, 13, 17}));  // 3 4 5 6 7
-    Counter myturn({4, 5, 6, 7, 8});
-    // REQUIRE(evaluate(&counter) == (6 - 7 + 1) + (2 - 7) + (3 - 7));
-    REQUIRE(defend(&counter, last_hand) == Hand(myturn));
+    Counter counter({3, 29, 41, 42, 43, 44, 46, 47});  // 3 10 K K K A A A
+    Hand last_hand(Counter({0, 1, 2, 22}));            // 3 3 3 8
+    REQUIRE(defend(&counter, last_hand) ==
+            Hand(Counter({3, 41, 42, 43})));  // K K K 3
   }
 }
 
 TEST_CASE("Strategy 3: defending absolute domination", "[0]") {
   evaluator = simple_evaluator;
-  defending_sequence = {boom_terminator_selector,
+  defending_sequence = {boom_terminator_selector, same_category_selector,
                         defending_absolute_dominant_selector, pass_selector};
 
   SECTION("Case 3.1") {
@@ -82,16 +77,17 @@ TEST_CASE("Strategy 3: defending absolute domination", "[0]") {
 
 TEST_CASE("Strategy 4: PASS", "[0]") {
   evaluator = simple_evaluator;
-  defending_sequence = {boom_terminator_selector,
+  defending_sequence = {boom_terminator_selector, same_category_selector,
                         defending_absolute_dominant_selector, pass_selector};
 
-  // // This condition ought to be pass! Needs Revision!
-  // SECTION("Case 4.1") {
-  //   Counter counter({28, 29, 30, 31, 50, 51});  // 10 10 10 10 2 2
-  //   Hand last_hand(Counter({0, 1, 2, 22}));     // 3 3 3 8
-  //   REQUIRE(defend(&counter, last_hand) ==
-  //           Hand(Counter({29, 30, 31, 50})));  // 10 10 10 2
-  // }
+  // This condition ought to be pass! Needs Revision!
+  SECTION("Case 4.1") {
+    Counter counter({28, 29, 30, 31, 50, 51});  // 10 10 10 10 2 2
+    Hand last_hand(Counter({0, 1, 2, 22}));     // 3 3 3 8
+    REQUIRE(defend(&counter, last_hand) ==
+            Hand(7, 1, 4));  // 10 10 10 10
+                             // I set the rule not to split the bomb
+  }
   SECTION("Case 4.2") {
     Counter counter({28, 29, 51});           // 10 10 2
     Hand last_hand(Counter({0, 1, 2, 22}));  // 3 3 3 8
